dea9f1e7cbbe89d0958fc6970f22bcc5869da680
alexa <alexanderredar1@gmail.com>
2021-11-01 05:11:41 +0300

    Finish Task #1

--- src/arrayRandom/arrayRandomLauncher.java
+++ src/arrayRandom/arrayRandomLauncher.java
@@ -2,6 +2,8 @@ package arrayRandom;
 
 import com.sun.jdi.Value;
 
+import java.sql.Struct;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.random.RandomGenerator;
@@ -9,16 +11,25 @@ import java.util.random.RandomGenerator;
 public class arrayRandomLauncher {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Array ownArray = new Array (scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        Array ownArray = new Array(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
         int[] array = arrayInitialize(ownArray);
         arrayToString(array);
+        minArrayValue(array);
+        averageArrayValue(array);
+        maxArrayValue(array);
     }
 
     private static int[] arrayInitialize(Array array) {
         int[] myArray = new int[array.getArraySize()];
         Random random = new Random();
         for (int i = 0; i < myArray.length; i++) {
-            myArray[i] = random.nextInt(array.getMaxN()) + array.getMinN();
+            boolean notInterval = true;
+            do {
+                myArray[i] = random.nextInt();
+                if (myArray[i] >= array.getMinN() && myArray[i] <= array.getMaxN()) {
+                    notInterval = false;
+                }
+            } while (notInterval == true);
         }
         return myArray;
     }
@@ -27,5 +38,25 @@ public class arrayRandomLauncher {
         for (int i = 0; i < array.length; i++) {
             System.out.println(i + 1 + ") " + array[i]);
         }
+    }
+
+    private static void minArrayValue(int[] array) {
+        Arrays.sort(array);
+        System.out.println("Минимальное значение: " + array[0]);
+    }
+
+    private static void maxArrayValue(int[] array) {
+        Arrays.sort(array);
+        System.out.println("Максимальное значение: " + array[array.length - 1]);
+    }
+
+    private static void averageArrayValue(int[] array) {
+        int avrN = 0;
+        for (int i = 0; i < array.length; i++) {
+            avrN += array[i];
+        }
+        avrN = avrN/array.length;
+
+        System.out.println("Среднее значение: " + avrN);
     }
 }
---
+++ src/mainLauncher.java
@@ -0,0 +1,7 @@
+import arrayRandom.*;
+
+public class mainLauncher {
+    public static void main(String[] args) {
+        arrayRandomLauncher.main(null);
+    }
+}
